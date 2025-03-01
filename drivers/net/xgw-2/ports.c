f7511bdf041e2dfc3a828677993d4fbbe44e9045
metaur0s <wolf.metaur0s@gmail.com>
2025-03-01 14:02:41 -0300

    1740848561

--- drivers/net/xgw-2/ports.c
+++ drivers/net/xgw-2/ports.c
@@ -3,11 +3,17 @@
 #include "ports.h"
 
 // TODO: SO APRENDER UM PATH SE TAL PORTA ESTIVER CONFIGURADA NELE
-static inline void ports_enable (const uint port)
-    { PORTS_W |= PORTS_B; }
+static inline void ports_enable (const uint port) {
 
-static inline void ports_disable (const uint port)
-    { PORTS_W &= ~PORTS_B; }
+    PORTS_W |= PORTS_B;
+}
 
-static inline ports_t ports_is_enabled (const uint port)
-    { return PORTS_W & PORTS_B; }
+static inline void ports_disable (const uint port) {
+
+    PORTS_W &= ~PORTS_B;
+}
+
+static inline ports_t ports_is_enabled (const uint port) {
+
+    return PORTS_W & PORTS_B;
+}
