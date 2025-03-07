46cd083d6989681175b481b534178cce8c1b1550
metaur0s <wolf.metaur0s@gmail.com>
2025-03-06 21:44:44 -0300

    1741308284

--- drivers/net/xgw/ping.h
+++ drivers/net/xgw/ping.h
@@ -1,6 +1,5 @@
 
-#define PING_SIZE_SYN    80
-#define PING_SIZE_NORMAL 72
+#define PING_SIZE 80
 #define PONG_SIZE 72
 
 #define PING_RANDOMS_N (K_LEN + 1)
@@ -10,16 +9,15 @@ struct ping_s {
     u64 rnd [K_LEN];
     u8 _ [7];
     u8 ver; // SENDER'S IKEYS INDEX BEING REGISTERED
-    u64 ctr; // THE PATH COUNTER
+    u64 ctr; // THE SENDER'S LCOUNTER
 };
 
 struct pong_s {
     u64 rnd [PONG_RANDOMS_N];
-    u64 ctr; // THE PING BEING ANSWERED
-};
+    u64 ctr; // THE SENDER'S LCOUNTER
+}; // NOTE: AO ENVIAR UM PONG, O PKT->COUNTER É O RCOUNTER SENDO RESPONDIDO
 
-BUILD_ASSERT(sizeof(ping_s)        == PING_SIZE_SYN);
-BUILD_ASSERT(offsetof(ping_s, ctr) == PING_SIZE_NORMAL);
-BUILD_ASSERT(sizeof(pong_s)        == PONG_SIZE);
+BUILD_ASSERT(sizeof(ping_s) == PING_SIZE);
+BUILD_ASSERT(sizeof(pong_s) == PONG_SIZE);
 
 BUILD_ASSERT(PING_SIZE != PONG_SIZE);
