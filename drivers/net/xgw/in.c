e71771e4524462ad8a4bffe040add790d2dcaaab
metaur0s <wolf.metaur0s@gmail.com>
2025-03-09 13:04:37 -0300

    1741536277

--- drivers/net/xgw/in.c
+++ drivers/net/xgw/in.c
@@ -14,12 +14,12 @@ static inline void pong_send (node_s* const node, path_s* const path, const pkt_
 
     uint stat;
 
-    skb_s* const oskb = alloc_skb(64 + PKT_SIZE + PKT_ALIGN_SIZE + PING_SIZE + 64, GFP_ATOMIC);
+    skb_s* const skb = alloc_skb(64 + PKT_SIZE + PKT_ALIGN_SIZE + PING_SIZE + 64, GFP_ATOMIC);
 
-    if (oskb) {
+    if (skb) {
 
         // TODO: USA O SKB_DATA ALIGNED
-        ping_s* const pong = SKB_DATA(oskb) + 64 + PKT_SIZE + PKT_ALIGN_SIZE;
+        ping_s* const pong = SKB_DATA(skb) + 64 + PKT_SIZE + PKT_ALIGN_SIZE;
 
         // GERA AS KEYS
         random64_n(PTR(pong), PING_RANDOMS_N, SUFFIX_ULL(CONFIG_XGW_RANDOM_PING));
@@ -41,11 +41,11 @@ static inline void pong_send (node_s* const node, path_s* const path, const pkt_
 
         // USA O RAW RTIME QUE RECEBEU
         // TODO: O ALIGN COM RANDOM TEM QUE SER COLOCADO FORA DO ENCAPSULATE, POIS NO CASO DO PING/PONG NAO VAMOS USAR
-        pkt_encapsulate(node, O_KEY_PONG, p_rtime, skel, oskb, pong, PING_SIZE);
+        pkt_encapsulate(node, O_KEY_PONG, p_rtime, skel, skb, pong, PING_SIZE);
 
-        oskb->ip_summed = CHECKSUM_NONE;
+        skb->ip_summed = CHECKSUM_NONE;
 
-        if (dev_queue_xmit(oskb))
+        if (dev_queue_xmit(skb))
             // FAILED TO SEND THE SKB
             // NOTE: THE SKB WAS ALREADY CONSUMED
             stat = PSTATS_O_PONG_SEND_FAILED;
@@ -57,6 +57,131 @@ static inline void pong_send (node_s* const node, path_s* const path, const pkt_
     // NOTE: WE WILL INFORM THE TOTAL SIZE SENT THROUGHT THE PHYSICAL INTERFACE
     atomic_add(&path->pstats[stat].bytes, skel->hsize + PKT_ALIGN_SIZE + PING_SIZE);
     atomic_inc(&path->pstats[stat].count);
+}
+
+static noinline void in_ping (node_s* const node, path_s* const path, pkt_s* const pkt) {
+
+    uint latency = atomic_get(&path->latency);
+    s64 tdiff    = atomic_get(&path->tdiff);
+    u64 rtime    = atomic_get(&path->rtime);
+
+    // HIS RAW TIME
+    const ping_s* const ping = PKT_DATA(pkt);
+
+    const u64 p_rtime = BE64(ping->time);
+
+    if (p_rtime < RTIME_MIN
+     || p_rtime > RTIME_MAX)
+        // INVALID RTIME
+        ret_path(PSTATS_I_RTIME_INVALID);
+
+    if (rtime >= RTIME_ESTABLISHED){
+        // JA CONHEÇO O TIME DELE, O DIFF E O LATENCY
+        if (p_rtime <= rtime)
+            // HIS RAW TIME CANNOT GO DOWN OR REPEAT
+            ret_path(PSTATS_I_RTIME_BACKWARDS);
+        // O QUANTO
+        //      (O RELOGIO DELE COMO ELE DIZ QUE ESTA (APROXIMADO))
+        //          (LEVANDO EM CONTA QUE ISTO FOI UM SNAPSHOT DELE HA *LATENCY* ATÉ RECEBERMOS)
+        // ESTÁ EM RELAÇÃO A
+        //      (O RELOGIO DELE COMO ELE DEVE ESTAR (APROXIMADO))
+        const s64 diff = (s64)(p_rtime + latency) - (s64)RTIME(now, tdiff);
+        // A IMPRECISÃO NÃO PODE SER TÃO GRANDE ASSIM:
+        // NOTE: CONSIDERAR PATH->LATENCY_VAR
+        if (diff > 2000) // PEER AFOBADO
+            ret_path(PSTATS_I_RTIME_SKEW_UP);
+        if (diff < -2000) // PEER LESADO
+            ret_path(PSTATS_I_RTIME_SKEW_DOWN);
+    }
+
+    if (i == I_KEY_PONG) {
+        // CONNECTING / ESTABLISHED
+
+        // p_ltime IS THE TIME WE SENT
+        // WE USE THE HALF, BECAUSE THIS TIME ELAPSED WAS TO GO AND GET BACK
+        latency = (3*latency + (now - p_ltime)/2) / 4;
+
+        // CAP TO CONFIGURED LIMITS
+        if (latency > path->latency_max)
+            latency = path->latency_max;
+        elif (latency < path->latency_min)
+              latency = path->latency_min;
+
+        // ELE NOS MANDOU O TIME DELE DE QUANDO ELE RECEBEU.
+        // MAS CONSIDERA O TIME QUE ELE TINHA QUANDO ENVIAMOS.
+        // E ENTAO PEGA A COMPARAÇÃO ENTRE *LOCAL TIME WHEN I SENT* COM *REMOTE TIME WHEN I SENT*
+        // LTIME_DIFF_RTIME(p_ltime + latency, p_rtime)
+        // LTIME_DIFF_RTIME(now, p_rtime + latency)
+        tdiff = (tdiff + LTIME_DIFF_RTIME(now, p_rtime + latency)) / 2;
+
+        if (!__atomic_compare_exchange_n(&path->pingSent, &p_ltime, 0, 0, __ATOMIC_SEQ_CST, __ATOMIC_RELAXED))
+            // THIS PONG WAS ALREADY PROCESSED, OR
+            // ANOTHER PING WAS SENT (AND A NEW PONG IS EXPECTED)
+            ret_path(PSTATS_I_PONG_RACED);
+
+        // CONFIRMOU QUE ESTE PONG RESPONDEU O ULTIMO PING ENVIADO
+        // LIMPOU ELE: NAO VAI ACEITAR OUTRO
+
+        // ESTE AQUI DEVERIA SER COMPARE, EXCHANGE, MAS:
+        // SÓ PODE ACONTECER UM RACE SE ENTRARMOS NESTE BLOCO E AO MESMO TEMPO O KEEPER GERAR UM NOVO path->pingSent,
+        // E A RESPOSTA VIR TELEPATICAMENTE E SER PROCESSADA AO MESMO TEMPO.
+        // O RESULTADO É QUE PODERIAMOS ESTAR ESCREVENDO ESTE P_RTIME ANTIGO POR CIMA DO NOVO.
+        // MAS DE QUALQUER JEITO, O NOVO É MAIOR DO QUE O ANTERIOR A ESTE, COMO CHECAMOS ACIMA.
+        __atomic_store_n(&path->latency, (u16)latency, __ATOMIC_RELAXED);
+        __atomic_store_n(&path->pongReceived, now,     __ATOMIC_RELAXED);
+        __atomic_store_n(&path->tdiff,        tdiff,   __ATOMIC_RELAXED);
+        __atomic_store_n(&path->rtime,       p_rtime,  __ATOMIC_SEQ_CST); // RTIME_CONNECTING / RTIME_ESTABLISHED -> RTIME_ESTABLISHED
+
+        // LEARN HIS INPUT KEYS (MY OUTPUT KEYS)
+        const uint ver = BE16(ping->ver);
+        const uint sec = BE16(ping->sec);
+
+        u64 K[K_LEN];
+
+        secret_derivate_random_as_key(node->secret[sec], ping->rnd, K);
+
+        // FAZ ISSO PRIMEIRO ANTES DE LIBERAR O PATH PARA ENVIAR
+        const uint o = __atomic_add_fetch(&node->oSave, 1, __ATOMIC_ACQUIRE) % O_KEYS_DYNAMIC;
+                                           node->oVersions[o] = ver;
+                                    memcpy(node->oKeys[o], K, sizeof(K));
+                         __atomic_store_n(&node->oUse, o,  __ATOMIC_RELEASE);
+
+        ret_path(PSTATS_I_PONG_GOOD);
+    }
+
+    // THIS IS A PING
+    pkt_s* skel; pkt_s temp_skel;
+
+    if (rtime == RTIME_LISTENING) {
+        // LISTENING - PATH HAS NO SKEL
+
+        if (i == I_KEY_SYN) {
+            // LEARN O PATH EM UM HEADER TEMPORARIO
+            skel = &temp_skel;
+        } elif (__atomic_compare_exchange_n(&path->rtime, &rtime, RTIME_ACCEPTING, 0, __ATOMIC_SEQ_CST, __ATOMIC_RELAXED)) {
+            // SYN-ACK
+            // LEARN ON PATH
+            skel = &path->skel;
+        } else // LOCK FAILED
+            ret_path(PSTATS_I_PING_GOOD_ACCEPT_RACED);
+
+        in_discover(path, skb, skel);
+
+        if (skel == &path->skel) {
+            // AGORA JA PODE USAR O PATH->SKEL
+            // LIBERA O KEEPER PARA ENVIAR PINGS
+            // LIBERA O OUT PARA ENVIAR DADOS
+            __atomic_store_n(&path->tdiff,  tdiff,  __ATOMIC_RELAXED);
+            __atomic_store_n(&path->rtime, p_rtime, __ATOMIC_SEQ_CST); // RTIME_ACCEPTING -> RTIME_ESTABLISHED
+        }
+    } else
+        // ESTABLISHED
+        skel = &path->skel;
+
+    // RESPONDE COM UM PONG
+    pong_send(node, path, skel, p_rtime, now);
+
+    ret_path(PSTATS_I_PING_GOOD);
 }
 
 // TODO: FIXME: PROTECT THE REAL SERVER TCP PORTS SO WE DON'T NEED TO BIND TO THE FAKE INTERFACE
@@ -279,126 +404,8 @@ _is_xgw:
     // IS AUTHENTIC (hash)
     // IS SYNCED (p_ltime)
 
-    if (i >= I_KEY_PING) {
-
-        // HIS RAW TIME
-        const ping_s* const ping = PKT_DATA(pkt);
-
-        const u64 p_rtime = BE64(ping->time);
-
-        if (p_rtime < RTIME_MIN
-         || p_rtime > RTIME_MAX)
-            // INVALID RTIME
-            ret_path(PSTATS_I_RTIME_INVALID);
-
-        if (rtime >= RTIME_ESTABLISHED){
-            // JA CONHEÇO O TIME DELE, O DIFF E O LATENCY
-            if (p_rtime <= rtime)
-                // HIS RAW TIME CANNOT GO DOWN OR REPEAT
-                ret_path(PSTATS_I_RTIME_BACKWARDS);
-            // O QUANTO
-            //      (O RELOGIO DELE COMO ELE DIZ QUE ESTA (APROXIMADO))
-            //          (LEVANDO EM CONTA QUE ISTO FOI UM SNAPSHOT DELE HA *LATENCY* ATÉ RECEBERMOS)
-            // ESTÁ EM RELAÇÃO A
-            //      (O RELOGIO DELE COMO ELE DEVE ESTAR (APROXIMADO))
-            const s64 diff = (s64)(p_rtime + latency) - (s64)RTIME(now, tdiff);
-            // A IMPRECISÃO NÃO PODE SER TÃO GRANDE ASSIM:
-            // NOTE: CONSIDERAR PATH->LATENCY_VAR
-            if (diff > 2000) // PEER AFOBADO
-                ret_path(PSTATS_I_RTIME_SKEW_UP);
-            if (diff < -2000) // PEER LESADO
-                ret_path(PSTATS_I_RTIME_SKEW_DOWN);
-        }
-
-        if (i == I_KEY_PONG) {
-            // CONNECTING / ESTABLISHED
-
-            // p_ltime IS THE TIME WE SENT
-            // WE USE THE HALF, BECAUSE THIS TIME ELAPSED WAS TO GO AND GET BACK
-            latency = (3*latency + (now - p_ltime)/2) / 4;
-
-            // CAP TO CONFIGURED LIMITS
-            if (latency > path->latency_max)
-                latency = path->latency_max;
-            elif (latency < path->latency_min)
-                  latency = path->latency_min;
-
-            // ELE NOS MANDOU O TIME DELE DE QUANDO ELE RECEBEU.
-            // MAS CONSIDERA O TIME QUE ELE TINHA QUANDO ENVIAMOS.
-            // E ENTAO PEGA A COMPARAÇÃO ENTRE *LOCAL TIME WHEN I SENT* COM *REMOTE TIME WHEN I SENT*
-            // LTIME_DIFF_RTIME(p_ltime + latency, p_rtime)
-            // LTIME_DIFF_RTIME(now, p_rtime + latency)
-            tdiff = (tdiff + LTIME_DIFF_RTIME(now, p_rtime + latency)) / 2;
-
-            if (!__atomic_compare_exchange_n(&path->pingSent, &p_ltime, 0, 0, __ATOMIC_SEQ_CST, __ATOMIC_RELAXED))
-                // THIS PONG WAS ALREADY PROCESSED, OR
-                // ANOTHER PING WAS SENT (AND A NEW PONG IS EXPECTED)
-                ret_path(PSTATS_I_PONG_RACED);
-
-            // CONFIRMOU QUE ESTE PONG RESPONDEU O ULTIMO PING ENVIADO
-            // LIMPOU ELE: NAO VAI ACEITAR OUTRO
-
-            // ESTE AQUI DEVERIA SER COMPARE, EXCHANGE, MAS:
-            // SÓ PODE ACONTECER UM RACE SE ENTRARMOS NESTE BLOCO E AO MESMO TEMPO O KEEPER GERAR UM NOVO path->pingSent,
-            // E A RESPOSTA VIR TELEPATICAMENTE E SER PROCESSADA AO MESMO TEMPO.
-            // O RESULTADO É QUE PODERIAMOS ESTAR ESCREVENDO ESTE P_RTIME ANTIGO POR CIMA DO NOVO.
-            // MAS DE QUALQUER JEITO, O NOVO É MAIOR DO QUE O ANTERIOR A ESTE, COMO CHECAMOS ACIMA.
-            __atomic_store_n(&path->latency, (u16)latency, __ATOMIC_RELAXED);
-            __atomic_store_n(&path->pongReceived, now,     __ATOMIC_RELAXED);
-            __atomic_store_n(&path->tdiff,        tdiff,   __ATOMIC_RELAXED);
-            __atomic_store_n(&path->rtime,       p_rtime,  __ATOMIC_SEQ_CST); // RTIME_CONNECTING / RTIME_ESTABLISHED -> RTIME_ESTABLISHED
-
-            // LEARN HIS INPUT KEYS (MY OUTPUT KEYS)
-            const uint ver = BE16(ping->ver);
-            const uint sec = BE16(ping->sec);
-
-            u64 K[K_LEN];
-
-            secret_derivate_random_as_key(node->secret[sec], ping->rnd, K);
-
-            // FAZ ISSO PRIMEIRO ANTES DE LIBERAR O PATH PARA ENVIAR
-            const uint o = __atomic_add_fetch(&node->oSave, 1, __ATOMIC_ACQUIRE) % O_KEYS_DYNAMIC;
-                                               node->oVersions[o] = ver;
-                                        memcpy(node->oKeys[o], K, sizeof(K));
-                             __atomic_store_n(&node->oUse, o,  __ATOMIC_RELEASE);
-
-            ret_path(PSTATS_I_PONG_GOOD);
-        }
-
-        // THIS IS A PING
-        pkt_s* skel; pkt_s temp_skel;
-
-        if (rtime == RTIME_LISTENING) {
-            // LISTENING - PATH HAS NO SKEL
-
-            if (i == I_KEY_SYN) {
-                // LEARN O PATH EM UM HEADER TEMPORARIO
-                skel = &temp_skel;
-            } elif (__atomic_compare_exchange_n(&path->rtime, &rtime, RTIME_ACCEPTING, 0, __ATOMIC_SEQ_CST, __ATOMIC_RELAXED)) {
-                // SYN-ACK
-                // LEARN ON PATH
-                skel = &path->skel;
-            } else // LOCK FAILED
-                ret_path(PSTATS_I_PING_GOOD_ACCEPT_RACED);
-
-            in_discover(path, skb, skel);
-
-            if (skel == &path->skel) {
-                // AGORA JA PODE USAR O PATH->SKEL
-                // LIBERA O KEEPER PARA ENVIAR PINGS
-                // LIBERA O OUT PARA ENVIAR DADOS
-                __atomic_store_n(&path->tdiff,  tdiff,  __ATOMIC_RELAXED);
-                __atomic_store_n(&path->rtime, p_rtime, __ATOMIC_SEQ_CST); // RTIME_ACCEPTING -> RTIME_ESTABLISHED
-            }
-        } else
-            // ESTABLISHED
-            skel = &path->skel;
-
-        // RESPONDE COM UM PONG
-        pong_send(node, path, skel, p_rtime, now);
-
-        ret_path(PSTATS_I_PING_GOOD);
-    }
+    if (i >= I_KEY_SYN)
+        ret_path(in_ping(node, path, pkt));
 
     // REPLAY/CORRUPTION/FORGING/EXPIRATION PROTECTION
     // NOTE: LEMBRANDO QUE O TEMPO TODO AMBOS FICAM AJUSTANDO O NODE->DIFF,
