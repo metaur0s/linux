bb06b596fa5801062e15bf2b6e0df4ae488cfc85
MetaurOs <wolf.metaur0s@gmail.com>
2025-03-10 07:24:26 -0300

    Update asserts.c

--- drivers/net/xgw/asserts.c
+++ drivers/net/xgw/asserts.c
@@ -1,3 +1,5 @@
+BUILD_ASSERT(XGW_MTU_OVERHEAD == (PKT_X_SIZE + PKT_ALIGN_SIZE));
+
 //
 BUILD_ASSERT(sizeof(cmd_arg_nname_t) == sizeof(((node_s*)NULL)->name));
 BUILD_ASSERT(sizeof(cmd_arg_pname_t) == sizeof(((path_s*)NULL)->name));
