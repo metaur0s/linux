15e6fa809a25b23b0c0992513f0ae2ccb1d08d57
metaur0s <wolf.metaur0s@gmail.com>
2025-03-13 07:46:27 -0300

    1741862787

--- drivers/net/xgw/cmd.c
+++ drivers/net/xgw/cmd.c
@@ -78,8 +78,8 @@ static ssize_t __cold_as_ice __optimize_size cmd (struct file *file, const char 
     if (C >= CMDS_N)
         FREE_CMD_ERR(INVALID_CMD_CODE);
 
-#if 0
-    printk("XGW: %s #%u\n", cmdNames[C], C);
+#if 1
+    printk("XGW: %s #%u SIZE %zd\n", cmdNames[C], C, size);
 #endif
 
     if (cmdSizes[C] > size)
