1553067c01af1c743529508f85e81c78cb67dcd7
MetaurOs <wolf.metaur0s@gmail.com>
2025-11-14 01:07:11 -0300

    Enable 2500BASE-X support in SFP bus driver

--- drivers/net/phy/sfp-bus.c
+++ drivers/net/phy/sfp-bus.c
@@ -124,8 +124,11 @@ static void sfp_module_parse_support(struct sfp_bus *bus,
 {
 	unsigned long *interfaces = bus->caps.interfaces;
 	unsigned long *modes = bus->caps.link_modes;
+#if 1
+	phylink_set(modes, 2500baseX_Full);
+	__set_bit(PHY_INTERFACE_MODE_2500BASEX, interfaces);
+#else
 	unsigned int br_min, br_nom, br_max;
-
 	/* Decode the bitrate information to MBd */
 	br_min = br_nom = br_max = 0;
 	if (id->base.br_nominal) {
@@ -300,20 +303,16 @@ static void sfp_module_parse_support(struct sfp_bus *bus,
 	 * 2500BASE-X, so we allow some slack here.
 	 */
 	if (linkmode_empty(modes) && br_nom) {
-#if 1
-		printk("HERE I AM, BEAUTIFUL WORLD!!!\n");
-#else
 		if (br_min <= 1300 && br_max >= 1200) {
 			phylink_set(modes, 1000baseX_Full);
 			__set_bit(PHY_INTERFACE_MODE_1000BASEX, interfaces);
 		}
-#endif
 		if (br_min <= 3200 && br_max >= 2500) {
 			phylink_set(modes, 2500baseX_Full);
 			__set_bit(PHY_INTERFACE_MODE_2500BASEX, interfaces);
 		}
 	}
-
+#endif
 	phylink_set(modes, Autoneg);
 	phylink_set(modes, Pause);
 	phylink_set(modes, Asym_Pause);
@@ -344,6 +343,10 @@ static void sfp_init_module(struct sfp_bus *bus,
 phy_interface_t sfp_select_interface(struct sfp_bus *bus,
 				     const unsigned long *link_modes)
 {
+#if 1
+	printk("!!! PHY_INTERFACE_MODE_2500BASEX\n");
+	return PHY_INTERFACE_MODE_2500BASEX;
+#else
 	if (phylink_test(link_modes, 25000baseCR_Full) ||
 	    phylink_test(link_modes, 25000baseKR_Full) ||
 	    phylink_test(link_modes, 25000baseSR_Full))
@@ -377,6 +380,7 @@ phy_interface_t sfp_select_interface(struct sfp_bus *bus,
 	dev_warn(bus->sfp_dev, "Unable to ascertain link mode\n");
 
 	return PHY_INTERFACE_MODE_NA;
+#endif
 }
 EXPORT_SYMBOL_GPL(sfp_select_interface);
 
