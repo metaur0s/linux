b40d4a4635264dfc7dc7762ce47e7758a3e3321d
MetaurOs <wolf.metaur0s@gmail.com>
2026-01-21 20:00:06 -0300

    Add eventpoll header to socket.c

--- net/socket.c
+++ net/socket.c
@@ -71,6 +71,7 @@
 #include <linux/ptp_classify.h>
 #include <linux/init.h>
 #include <linux/poll.h>
+#include <linux/eventpoll.h> // speedyb0y
 #include <linux/cache.h>
 #include <linux/module.h>
 #include <linux/highmem.h>
