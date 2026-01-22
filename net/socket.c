57f783244661b500d81413b6c24cc604698c9b58
MetaurOs <wolf.metaur0s@gmail.com>
2026-01-21 21:49:07 -0300

    Reorder socket options enumeration for clarity

--- net/socket.c
+++ net/socket.c
@@ -2347,16 +2347,17 @@ typedef __u8  u8;
 typedef unsigned int uint;
 
 enum : uint {
-    MYSOCKET_OPTS__RCV_SIZE  = 1U << 0,
-    MYSOCKET_OPTS__SND_SIZE  = 1U << 1,
-    MYSOCKET_OPTS__ITFC      = 1U << 2,
-    MYSOCKET_OPTS__MARK      = 1U << 3,
-    MYSOCKET_OPTS__NODELAY   = 1U << 4,
-    MYSOCKET_OPTS__QUICKACK  = 1U << 5,
-    MYSOCKET_OPTS__SYNCNT    = 1U << 6,
-    MYSOCKET_OPTS__KEEPALIVE = 1U << 7,
-    MYSOCKET_OPTS__BIND      = 1U << 8,
-    MYSOCKET_OPTS__CONNECT   = 1U << 9,
+    MYSOCKET_OPTS__EPOLL     = 1U <<  0,
+    MYSOCKET_OPTS__RCV_SIZE  = 1U <<  1,
+    MYSOCKET_OPTS__SND_SIZE  = 1U <<  2,
+    MYSOCKET_OPTS__ITFC      = 1U <<  3,
+    MYSOCKET_OPTS__MARK      = 1U <<  4,
+    MYSOCKET_OPTS__NODELAY   = 1U <<  5,
+    MYSOCKET_OPTS__QUICKACK  = 1U <<  6,
+    MYSOCKET_OPTS__SYNCNT    = 1U <<  7,
+    MYSOCKET_OPTS__KEEPALIVE = 1U <<  8,
+    MYSOCKET_OPTS__BIND      = 1U <<  9,
+    MYSOCKET_OPTS__CONNECT   = 1U << 10,
 };
 
 typedef struct mysocket_opts_params_s {
